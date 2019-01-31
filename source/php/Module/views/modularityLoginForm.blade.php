8f0b7d6679d47c0dd571f04e73b2a7b27e60648a
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2019-01-31 15:06:28 +0100

    Changes to secondary style

--- source/php/Module/views/modularityLoginForm.blade.php
+++ source/php/Module/views/modularityLoginForm.blade.php
@@ -1,13 +1,12 @@
 <div class="grid">
-<div class="grid-md-12" id="login-box">
-    <div class="box box-panel box-panel-primary">
-        @if (!$hideTitle && !empty($post_title))<h4 class="box-title">{!! apply_filters('the_title', $post_title) !!}</h4>@endif
-        <div class="box-content">
-            <div id="modularity-login-form"></div>
+    <div class="grid-md-12" id="login-box">
+        <div class="box box-panel box-panel-secondary">
+            @if (!$hideTitle && !empty($post_title))<h4 class="box-title">{!! apply_filters('the_title', $post_title) !!}</h4>@endif
+            <div class="box-content">
+                <div id="modularity-login-form"></div>
+            </div>
         </div>
-
     </div>
-</div>
 </div>
 
 
